2918375f703ca3fc062e17037533083d77a478a1
[Menno Lont] <[menno_lont@live.nl]>
2019-01-30 12:10:33 +0100

    Added datepicker

--- Android/app/src/main/java/com/example/menno_000/programmeerproject/MainActivity.java
+++ Android/app/src/main/java/com/example/menno_000/programmeerproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.menno_000.programmeerproject;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,13 +24,15 @@ import java.util.Date;
 
 import static java.lang.Integer.parseInt;
 
-public class MainActivity extends AppCompatActivity/* implements FoodRequest.Callback*/ {
+public class MainActivity extends AppCompatActivity {
 
     StoredFoodDatabase storedFoodDatabase;
     Class activity;
     TextView dateView;
     ArrayList<ArrayList> listItems = new ArrayList<>();
     Integer totalCal;
+    DatePickerDialog dpd;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,18 +52,21 @@ public class MainActivity extends AppCompatActivity/* implements FoodRequest.Cal
         // Buttons
         Button graphButton = findViewById(R.id.mainGraphButton);
         Button userButton = findViewById(R.id.mainUserButton);
+        TextView datePicker = findViewById(R.id.mainDatePicker);
+
 
         // Listeners
         graphButton.setOnClickListener(new MainActivity.ButtonClickListener());
         userButton.setOnClickListener(new MainActivity.ButtonClickListener());
+        datePicker.setOnClickListener(new MainActivity.DatePickListener());
 
         // Date
         dateView            = findViewById(R.id.mainDatePicker);
         Date date           = Calendar.getInstance().getTime();
-        String day          = (String) DateFormat.format("EEEE", date);
-        String daynumber    = (String) DateFormat.format("dd",   date);
-        String monthString  = (String) DateFormat.format("MMMM",  date);
-        dateView.setText(day + ", " + daynumber + " " + monthString);
+        String day          = (String) DateFormat.format("dd",   date);
+        String monthString  = (String) DateFormat.format("M",  date);
+        String year         = (String) DateFormat.format("yy", date);
+        dateView.setText(day + "-" + monthString + "-" + year);
 
         // Total
         TextView totalView = findViewById(R.id.main_total);
@@ -81,6 +88,30 @@ public class MainActivity extends AppCompatActivity/* implements FoodRequest.Cal
                     goToNext("user");
                     break;
             }
+        }
+    }
+
+
+    // Listener for the buttons, go to the next/previous screen
+    public class DatePickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+
+            calendar = Calendar.getInstance();
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            int month = calendar.get(Calendar.MONTH);
+            int year = calendar.get(Calendar.YEAR);
+
+            dpd = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+
+                    TextView tempDatePicker = findViewById(R.id.mainDatePicker);
+                    tempDatePicker.setText(d + "-" + m+1 + "-" + y);
+                }
+            }, year, month, day);
+            dpd.show();
         }
     }
 
